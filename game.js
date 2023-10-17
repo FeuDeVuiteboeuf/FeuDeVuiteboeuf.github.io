5fc211c37f5ec71d789a8ac4e93b0dc1308ca8dc
TornareX <tornare.xavier@gmail.com>
2023-10-17 22:17:53 +0200

    Added score and song background

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ document.addEventListener('keydown', function (event) {
 
     // Déterminez quelle lumière doit être modifiée en fonction de la touche pressée
     let lumiere = mapToucheToLumiere(event.key.toUpperCase());
+    urHistory.push(event.key.toLowerCase());
 
 
     // Ajoutez ou retirez la classe "red" en fonction de la touche pressée
@@ -151,10 +152,10 @@ function mapToucheToNote(key) {
         case 3: note = 370; break;
         case 4: note = 329.6; break;
         case 5: note = 349.2; break;
-        case 6: note = 370; break;
+        case 6: note = 523.25; break;
         case 7: note = 392; break;
         case 8: note = 440; break;
-        case 9: note = 493.9; break;
+        case 9: note = 466.16; break;
         default: return; // Si la touche n'est pas reconnue, ne rien faire
     }
     return note;
@@ -162,10 +163,31 @@ function mapToucheToNote(key) {
 
 let oscillator = new Array(9);
 let context = null;
+const whyDoYouCheat = ['q', 's', 's', 'd', 's', 'q', 's', 's',
+    's', 's', 'e', 'd', 'c', 'd', 'e', 's', 'e', 'e',
+    'q', 's', 's', 'd', 's', 'q', 's', 's', 's', 's',
+    'e', 'd', 'c', 'd', 'e', 'x', 's'];
+let urHistory = [];
+let gameStarted = false;
+let finalScore = 0;
 
 document.getElementById('danceButton').addEventListener('click', function () {
-    context = new (window.AudioContext || window.webkitAudioContext)();
-    document.getElementById('traffic').style.display = 'flex';
+    if (gameStarted) {
+        submitSong();
+        displayScore(finalScore);
+        document.getElementById('reset-button').style.display = 'inline-block';
+    }
+    else {
+        context = new (window.AudioContext || window.webkitAudioContext)();
+        document.getElementById('traffic').style.display = 'flex';
+        document.getElementById('danceButton').textContent = 'Submit my song to Vuiteboeuf\'s Fire';
+        gameStarted = true;
+        startBackgroundSong();
+    }
+});
+
+document.getElementById('reset-button').addEventListener('click', function () {
+    resetGame();
 });
 
 
@@ -182,4 +204,85 @@ function stopNote(lumiere) {
     oscillator[lumiere].stop();
 }
 
+function submitSong() {
+    finalScore = coefficientDice(whyDoYouCheat, urHistory);
+}
+
+function coefficientDice(reponse, proposition) {
+    const compteurReponse = new Map();
+
+    reponse.forEach(lettre => {
+        compteurReponse.set(lettre, (compteurReponse.get(lettre) || 0) + 1);
+    });
+
+    let correspondances = 0;
+
+    proposition.slice(0, 35).forEach(lettre => {
+        if (compteurReponse.has(lettre) && compteurReponse.get(lettre) > 0) {
+            correspondances++;
+            compteurReponse.set(lettre, compteurReponse.get(lettre) - 1);
+        }
+    });
+
+    const score = (correspondances / reponse.length) * 100;
+
+    return Math.min(score, 100);
+}
+
+
+function displayScore(scoreFinal) {
+    const elementScore = document.getElementById('score'); // Assurez-vous d'avoir un élément avec l'ID 'score' dans votre HTML
+    const dureeAnimation = 3000; // Durée totale de l'animation en millisecondes (3 secondes dans cet exemple)
+    const intervalleChangement = 50; // Intervalle de changement en millisecondes (50 ms dans cet exemple)
+    let timer;
+
+    function changerNombreAleatoire() {
+        const nombreAleatoire = Math.random() * 100; // Génère un nombre aléatoire entre 0 et 100
+        elementScore.textContent = Math.round(nombreAleatoire);
+        timer = setTimeout(changerNombreAleatoire, intervalleChangement);
+    }
+
+    setTimeout(function () {
+        clearTimeout(timer);
+        elementScore.textContent = Math.round(scoreFinal);
+    }, dureeAnimation);
+
+    changerNombreAleatoire();
+}
+
+function resetGame() {
+    urHistory = [];
+    gameStarted = false;
+    finalScore = 0;
+
+    document.getElementById('traffic').style.display = 'none';
+    document.getElementById('danceButton').textContent = 'Make me dance';
+    gameStarted = false;
+
+    const elementScore = document.getElementById('score'); // Assurez-vous d'avoir un élément avec l'ID 'score' dans votre HTML
+    elementScore.textContent = finalScore;
+
+    document.getElementById('reset-button').style.display = 'none';
+}
+
+function startBackgroundSong() {
+    // Créez un objet AudioContext
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+    // Chargez le fichier audio
+    const audioSource = 'loopDrums.wav';
+
+    const audioBufferSource = audioContext.createBufferSource();
+
+    fetch(audioSource)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+        .then(audioBuffer => {
+            audioBufferSource.buffer = audioBuffer;
+            audioBufferSource.loop = true; // Activez la boucle
+            audioBufferSource.connect(audioContext.destination);
+            audioBufferSource.start();
+        })
+        .catch(error => console.error('Erreur lors du chargement du fichier audio:', error));
+}
 
